9f30f826fa0d4c091be591a68635efb69c3af279
Arexmarx <phannguyenmanhcuong4662@gmail.com>
2025-07-14 20:45:06 +0700

    merge

---
+++ Assets/Script/AudioManger.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource musicSource;
+    public AudioClip menuMusic;
+    public AudioClip levelMusic;
+
+    void Awake()
+    {
+        // Singleton pattern: Đảm bảo chỉ có 1 AudioManager tồn tại
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+            Destroy(gameObject); 
+            return;
+        }
+    }
+
+    void Start()
+    {
+        PlayMenuMusic(); 
+    }
+
+    public void PlayMenuMusic()
+    {
+        PlayMusic(menuMusic);
+    }
+
+    public void PlayLevelMusic()
+    {
+        PlayMusic(levelMusic);
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        if (musicSource.clip == clip) return;
+
+        musicSource.clip = clip;
+        musicSource.loop = true;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+}
---
+++ Assets/Script/PauseManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    private bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused) ResumeGame();
+            else PauseGame();
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Debug.Log("Game Paused");
+        Time.timeScale = 0;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
